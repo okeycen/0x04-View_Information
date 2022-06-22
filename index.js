b47b0ccf68324c19bc2649885ca1be33fe74825c
okeycen <okeycen2m@gmail.com>
2022-06-22 15:21:10 +0100

    first commit message

---
+++ index.js
@@ -0,0 +1,8 @@
+function display(){
+    let name="Okechukwu Philips Cen"
+    let height= "5ft"
+    let country= "Nigeria"
+
+    let myInfo= "My Name is "+name+"\n"+"I am "+height+" tall\n"+"I am from "+country
+    return myInfo
+}
